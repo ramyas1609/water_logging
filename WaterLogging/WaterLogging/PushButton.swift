97b5e9795916831bf83448c9909cfdd14144e154
ramya <ramya_sriraman@apple.com>
2020-07-03 15:58:22 -0700

    Customize add and update buttons

---
+++ WaterLogging/WaterLogging/PushButton.swift
@@ -0,0 +1,73 @@
+//
+//  PushButton.swift
+//  WaterLogging
+//
+//  Created by Ramya on 7/3/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+
+class PushButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+    var fillColor : UIColor = .green
+    private struct Constants {
+        static let plusLineWidth: CGFloat = 3.0
+        static let plusButtonScale: CGFloat = 0.6
+        static let halfPointShift: CGFloat = 0.5
+    }
+
+    private var halfWidth: CGFloat {
+      return bounds.width / 2
+    }
+
+    private var halfHeight: CGFloat {
+      return bounds.height / 2
+    }
+
+    // half width and half height bring you to the center of the button
+
+    override func draw(_ rect: CGRect) {
+      let path = UIBezierPath(ovalIn: rect)
+      fillColor.setFill()
+      path.fill()
+
+    // The horizontal and vertical line intersect in the middle of the button.
+    // The lenght of the lines is 1/2 the minimum height or width times a constant.
+    // The constant allows us adjust the size of the plus sign.
+    let plusWidth = min(bounds.width, bounds.height) * Constants.plusButtonScale
+    let halfPlusWidth = plusWidth / 2
+
+    let plusPath = UIBezierPath()
+    plusPath.lineWidth = Constants.plusLineWidth
+
+    // Move initial point to left most point of the horizontal line
+    plusPath.move(to: CGPoint(
+      x: halfWidth - halfPlusWidth,
+      y: halfHeight))
+
+    //Add a point to the path at the end of the stroke
+    plusPath.addLine(to: CGPoint(
+      x: halfWidth + halfPlusWidth,
+      y: halfHeight))
+
+    plusPath.move(to: CGPoint(x:halfWidth , y: halfHeight + halfPlusWidth))
+
+    plusPath.addLine(to: CGPoint(x:halfWidth, y:halfHeight - halfPlusWidth))
+
+    //set the stroke color
+    UIColor.white.setStroke()
+    //draw the stroke
+    plusPath.stroke()
+
+    }
+
+}
--- WaterLogging/WaterLogging/TrackWaterViewController.swift
+++ WaterLogging/WaterLogging/TrackWaterViewController.swift
@@ -7,34 +7,47 @@
 import UIKit
 
 class TrackWaterViewController: UIViewController {
-    
-    private let addWaterButton = UIButton()
+
+    private let addWaterButton = PushButton()
     private let updateGoalButton = UIButton()
-    
+    private let maxNumberOfGlasses: Int = 10
+
+    var counter: Int = 0 {
+      didSet {
+        if counter <= maxNumberOfGlasses {
+          //the view needs to be refreshed
+        }
+      }
+    }
+
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
         setUp()
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
     // Set Up
-    
+
     private func setUp() {
-        addWaterButton.setTitle("Add 8 oz Water", for: .normal)
-        updateGoalButton.setTitle("Update Daily Goal", for: .normal)
-        addWaterButton.addTarget(self, action: #selector(addWaterButtonPressed), for: .touchUpInside)
-        updateGoalButton.addTarget(self, action: #selector(goalButtonPressed), for: .touchUpInside)
-        
         view.backgroundColor = .systemBackground
-        addWaterButton.backgroundColor = .black
-        updateGoalButton.backgroundColor = .black
+
+        // Set up update goal button
+        updateGoalButton.setTitle("Set Daily Goal", for: .normal)
+        updateGoalButton.addTarget(self, action: #selector(goalButtonPressed), for: .touchUpInside)
+        updateGoalButton.backgroundColor = .lightGray
+        updateGoalButton.layer.cornerRadius = 5
+        updateGoalButton.layer.borderWidth = 1
+        updateGoalButton.layer.borderColor = UIColor.black.cgColor
         
+        // Set up add water button
+        addWaterButton.addTarget(self, action: #selector(addWaterButtonPressed), for: .touchUpInside)
+
         setUpConstraints()
     }
-    
+
     private func setUpConstraints() {
         let container = UIView()
         addWaterButton.translatesAutoresizingMaskIntoConstraints = false
@@ -46,16 +59,22 @@ class TrackWaterViewController: UIViewController {
         view.addSubview(container)
         
         // Buttons constraints
-        let addWaterButtonConstraints = [addWaterButton.topAnchor.constraint(equalTo: container.topAnchor),
-                                         addWaterButton.leadingAnchor.constraint(equalTo: container.leadingAnchor),
-                                         addWaterButton.trailingAnchor.constraint(equalTo: container.trailingAnchor),]
+        let addWaterButtonConstraints = [
+            addWaterButton.topAnchor.constraint(equalTo: container.topAnchor),
+            addWaterButton.leadingAnchor.constraint(equalTo: container.leadingAnchor),
+            addWaterButton.trailingAnchor.constraint(equalTo:container.trailingAnchor),
+            addWaterButton.widthAnchor.constraint(equalToConstant: 120),
+            addWaterButton.heightAnchor.constraint(equalToConstant: 120)
+        ]
         
         NSLayoutConstraint.activate(addWaterButtonConstraints)
         
-        let updateGoalButtonConstraints = [updateGoalButton.topAnchor.constraint(equalTo: addWaterButton.bottomAnchor, constant: 10),
-                                           updateGoalButton.leadingAnchor.constraint(equalTo: container.leadingAnchor),
-                                           updateGoalButton.trailingAnchor.constraint(equalTo: container.trailingAnchor),
-                                           updateGoalButton.bottomAnchor.constraint(equalTo: container.bottomAnchor)]
+        let updateGoalButtonConstraints = [
+            updateGoalButton.topAnchor.constraint(equalTo: addWaterButton.bottomAnchor, constant: 60),
+            updateGoalButton.leadingAnchor.constraint(equalTo: container.leadingAnchor),
+            updateGoalButton.trailingAnchor.constraint(equalTo: container.trailingAnchor),
+            updateGoalButton.bottomAnchor.constraint(equalTo: container.bottomAnchor),
+        ]
         
         NSLayoutConstraint.activate(updateGoalButtonConstraints)
         
@@ -71,17 +90,17 @@ class TrackWaterViewController: UIViewController {
         NSLayoutConstraint.activate(containerConstraints)
         
     }
-    
+
     // Actions
-    
+
     @objc private func addWaterButtonPressed() {
-        print("Add water button pressed")
+        counter = counter + 1
+        print("Counter ", counter)
     }
-    
+
     @objc private func goalButtonPressed() {
         print("Goal button pressed")
     }
-    
 
 }
 
